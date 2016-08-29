0a83c415e866fc4ca185b3419e02400a0262050f
Ross Dyson <dyson.ross@gmail.com>
2016-08-28 18:03:56 -0700

    making ajax requests. implementing homepage routes, actions and reducers

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PostsIndex extends React.Component {
+    componentWillMount() {
+        console.log('WHAT UP CRIP!');
+    }
+
+    render() {
+        return (
+            <div>
+                WHAT UP BLOOD
+            </div>
+        )
+    }
+}
+
+export default PostsIndex;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
+import HomePostReducer from './reducer_homeposts'
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    posts: HomePostReducer
 });
 
 export default rootReducer;
